1c68a0e77d70c3188c0e28900de0465245fa15ed
Christopher Spraggett <cspraggett@gmail.com>
2020-01-15 20:24:01 -0500

    refactored fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -10,8 +10,15 @@ const rl = readline.createInterface({
 
 request(site, (error, response, body) => {
   if (error) {
-    throw new Error(error.message);
+    console.log("Error, request not successful");
+    process.exit();
   }
+  // if (response.statusCode < 200 || response.statusCode >= 300) {
+  //   console.log(
+  //     `Request was unsuccessful, Error code: ${response.statusCode} `
+  //   );
+  //   process.exit();
+  // }
   fs.access(path, fs.constants.F_OK, err => {
     if (!err) {
       rl.question(`${path} already exists, press 'y' to erase it: `, answer => {
